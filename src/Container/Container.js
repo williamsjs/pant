f67658ce671a13ae288b6eb58aeb7a4a8ef6e162
williamsjs <williamsjs1224@gmail.com>
2018-03-14 20:57:15 -0400

    import constant arr for dog types

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import List from '../shared/List/List';
 import Loading from '../shared/Loading';
+import DogCategories from '../shared/DogCategories';
 import './Container.css';
 
 class Container extends Component {
@@ -10,24 +11,20 @@ class Container extends Component {
   }
 
   componentDidMount() {
-    fetch(`https://dog.ceo/api/breeds/list/all`)
-      .then(items => items.json())
-      .then(items => Object.keys(items.message))
-      .then(categories => {
-        const listItems = [];
-        Promise.all(
-          categories.map((category, index) => {
-            listItems.push({title: category, id: index + 1});
-            return fetch(`https://dog.ceo/api/breed/${category}/images/random`)
-          })
-        ).then(requests => {
-          Promise.all(requests.map(req => req.json()))
-            .then(items => {
-              items.forEach((item, index) => listItems[index].img = item.message);
-              this.setState({list: listItems, loading: false})
-            });
+    const listItems = [];
+    Promise.all(
+      DogCategories.map((category, index) => {
+        listItems.push({title: category, id: index + 1});
+        return fetch(`https://dog.ceo/api/breed/${category}/images/random`)
+      })
+    ).then(requests => {
+      Promise.all(requests.map(req => req.json()))
+        .then(items => {
+          items.forEach((item, index) => listItems[index].img = item.message);
+          this.setState({list: listItems, loading: false})
         });
-      });
+    });
+      
   }
 
 
