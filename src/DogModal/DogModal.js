22022c05859e14640ed8af536cea95bbc12e9dc8
williamsjs <williamsjs1224@gmail.com>
2018-04-20 20:08:38 -0400

    add custom styles to modal

--- src/DogModal/DogModal.js
+++ src/DogModal/DogModal.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Slider from 'react-slick';
 import Modal from 'react-modal';
+import './DogModal.css';
+
+Modal.setAppElement('#root')
 
 const DogModal = ({modalIsOpen, closeModal, fetchDogs, currentBreedPics}) => {
   var settings = {
     infinite: true,
     centerMode: true,
     speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 2
+    slidesToShow: 1,
+    slidesToScroll: 1
   };
 
   return (
-    <Modal ariaHideApp={false} isOpen={modalIsOpen} onAfterOpen={fetchDogs} onRequestClose={closeModal}>
+    <Modal ariaHideApp={false} className="Modal" overlayClassName="Overlay" isOpen={modalIsOpen} onAfterOpen={fetchDogs} onRequestClose={closeModal}>
       <Slider {...settings} >
         {currentBreedPics.map((pic, i) => <div key={i}><img style={{width: '100%', height: '400px'}} src={pic} /></div>)}
       </Slider>
